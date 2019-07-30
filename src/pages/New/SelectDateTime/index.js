8f9c5c6dcbeb463250235f0e6edccc7d468ada4d
saaddalida <dalidassaad@gmail.com>
2019-07-30 15:38:14 -0300

    Listagem Prestadores

---
+++ src/pages/New/SelectDateTime/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Background from '~/components/Background';
+// import { Container } from './styles';
+
+export default function SelectDateTime() {
+  return <Background />;
+}
+
+SelectDateTime.navigationOptions = {
+  title: 'Selecione o horário',
+};
